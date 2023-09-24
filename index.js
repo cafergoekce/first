ad8ece94a2d7342aff63524bcf517b0b5824d040
Cafer Goekce <goekcecafertayyar@gmail.com>
2023-09-24 15:12:18 +0200

    vercele göre ayarlandi

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const app = require('express')();
 
 app.get('/', (req, res) => {
-  res.send('Hello World! CAFER Selam')
-})
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item:`);
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.get('/api/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+module.exports = app;
