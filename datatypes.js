bed0415665f6528800e7b3195851ea0fc57babf6
Cafer Goekce <goekcecafertayyar@gmail.com>
2023-09-26 16:28:45 +0200

    assignments

--- datatypes.js
+++ datatypes.js
@@ -14,3 +14,32 @@ console.log("Data type of boolVariable:", typeof boolVariable); // boolean
 console.log("Data type of undefinedVariable:", typeof undefinedVariable); //undefined
 console.log("Data type of nullVariable:", typeof nullVariable); // object
 
+// declare four variables without assigning values
+
+let gel 
+let ab15
+let %pin 
+let fed
+
+// declare four variables with assigned values
+
+let var1, var2, var3, var4
+var1 = 2, var2 = 35, var3 = 63, var4 = 8
+
+// declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+firstName = "Jack",
+lastName = "Falky", 
+maritalStatus = "married", 
+country = "France", 
+age = 39
+
+// declare variables to store your first name, last name, marital status, country and age in a single line
+
+let firstName = "Jack", lastName = "Falky", maritalStatus = "married", country = "France", age = 39
+
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console
+
+
+
