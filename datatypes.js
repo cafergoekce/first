97180d2494440f0d78f8a38b88605f046e47cc1f
Cafer Goekce <goekcecafertayyar@gmail.com>
2023-09-26 15:55:29 +0200

    import

---
+++ datatypes.js
@@ -0,0 +1,16 @@
+// import variables from the 'variables.js' file
+
+const {
+    strVariable,
+    boolVariable,
+    undefinedVariable,
+    nullVariable
+} = require('./variables');
+
+// check the data types of each variable using typeof
+
+console.log("Data type of strVariable:", typeof strVariable); // string
+console.log("Data type of boolVariable:", typeof boolVariable); // boolean
+console.log("Data type of undefinedVariable:", typeof undefinedVariable); //undefined
+console.log("Data type of nullVariable:", typeof nullVariable); // object
+
