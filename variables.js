29ca20f347fc9c101fdbe042dc130e230656a796
Cafer Goekce <goekcecafertayyar@gmail.com>
2023-09-26 14:46:28 +0200

    commit

---
+++ variables.js
@@ -0,0 +1,21 @@
+//comments can make code readable
+// Welcome to the 30DaysOfJavaScript
+/* comments can make code readable,
+   easy to reuse and informative */
+let a = ("number")
+number = "9";
+
+const strVariable = "cafer"; // string
+const boolVariable = true; // boolean
+let undefinedVariable; // undefined
+const nullVariable = null; // null
+
+// Export these variables so they can be used in another file
+
+module.exports= {
+    strVariable
+    boolVariable
+    undefinedVariable
+    nullVariable
+}
+
