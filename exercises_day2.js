8d55cd4f5a493fc8411ae48113d9ecacf367dffa
Cafer Goekce <goekcecafertayyar@gmail.com>
2023-09-27 16:53:36 +0200

    updates

--- exercises_day2.js
+++ exercises_day2.js
@@ -8,8 +8,95 @@ console.log(challenge);
 
 // 3-)
 
-let o1 = 'print'
+let o1 = 'print';
 console.log(o1.length)
 
 // 4-)
+
+console.log(challenge.toUpperCase(), o1.toUpperCase())
+
+// 5-)
+
+console.log(challenge.toLowerCase(), o1.toLowerCase())
+
+// 6-)
+
+let string1 = 'codeitanytime';
+console.log(string1.substring(4,13))
+
+// 7-)
+
+let phrase = challenge.substring(challenge.indexOf('Days'), challenge.length);
+console.log(phrase); // This will print 'Days Of JavaScript' to the console
+
+// 8-)
+
+console.log(string.includes('Script')) // this will print 'True'
+
+// 9-)
+
+console.log(string.split(' ')) // This will print '[Javasript]'
+
+// 10-)
+
+console.log(challenge.split(' ')) // This will print '[ "30", "Days", "of", "Javascript" ]'
+
+// 11-)
+
+let companys = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+console.log(companys.split(' '))    // This will print '['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']'
+
+// 12-)
+
+console.log(challenge.replace('Javascript', 'Python')) // This will print '30 Days of Python'
+
+// 13-)
+
+console.log(challenge.charAt(15))   // This will print 's'
+
+// 14-)
+
+console.log(challenge.charCodeAt('J'))  // This will print 51
+
+// 15-)
+
+console.log(challenge.indexOf('a')) // This will print 4
+
+// 16-)
+
+console.log(challenge.lastIndexOf('a')) // This will print 14
+
+// 17-)
+
+let sentence = 'You cannot end a sentence with because because because is a conjunction';
+console.log(sentence.indexOf('because')) // This will print 31
+
+// 18-)
+
+console.log(sentence.lastIndexOf('because')) // This will print 47
+
+// 19-)
+
+console.log(sentence.search('because')) // This will print 31
+
+// 20-)
+
+let trimmedChg = challenge.trim(); 
+console.log(trimmedChg);      // this will print '30 Days of Javascript'
+
+// 21-)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
